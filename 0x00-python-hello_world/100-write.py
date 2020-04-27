05d37455fe5c11da9a3051f65101284cc8318aff
Carter C <cclements3150@gmail.com>
2020-04-27 15:44:11 -0400

    added 100 - advanced assignment

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import sys
+
+ext = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+sys.stderr.write("Write a Python script that prints exactly {}".format(ext))
+sys.exit(1)
