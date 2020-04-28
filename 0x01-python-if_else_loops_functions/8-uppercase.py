2b9c9feedee132405aab99258932d6fb0ac6eddb
Carter C <cclements3150@gmail.com>
2020-04-28 12:24:26 -0400

    Prints a string in all uppercase (without using .upper)

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def uppercase(str):
+    LOUD_STR = ""
+    for i in str:
+        if ord(i) > 96 and ord(i) < 123:
+            LOUD_STR = LOUD_STR + chr(ord(i) - 32)
+        else:
+            LOUD_STR = LOUD_STR + i
+    print("{}".format(LOUD_STR))
