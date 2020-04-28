ddaf269bd6b9c5be2f038dd7be67c374e36b60cc
Carter C <cclements3150@gmail.com>
2020-04-28 11:22:33 -0400

    Checks if a last digit is below 0, or 6

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    lastd = (number * -1) % 10
+else:
+    lastd = number % 10
+print("Last digit of {} is {} and is ".format(number, lastd), end="")
+
+if lastd > 5:
+    print("greater than 5")
+elif lastd < 6 and not 0:
+    print("less than size and not 0")
+else:
+    print("0")
