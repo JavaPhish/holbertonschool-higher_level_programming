118a03f12abdcf0e9e30983ba7d12b33e35644f9
Carter C <cclements3150@gmail.com>
2020-04-28 14:06:23 -0400

    Fixed logic issue

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,19 +12,18 @@
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *node = *(head);
+	listint_t *new;
 	listint_t *temp;
 
 	while (node->next != NULL && node->next->n < number)
 		node = node->next;
 
+	new = malloc(sizeof(listint_t));
+	new->n = number;
+
 	temp = node->next;
-	node->next = NULL;
+	node->next = new;
+	new->next = temp;
 
-	add_nodeint_end(&node, number);
-
-	node = node->next;
-	node->next = temp;
-	free(temp);
-
-	return node;
+	return new;
 }
