7ffb6c1c025f5bce0f48889c5b37b7fb917dd3fa
Carter C <cclements3150@gmail.com>
2020-04-28 13:45:11 -0400

    Fixed logic

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 for frst in range(0, 9):
-    for scnd in range(frst + 1, 9):
-        print("{}{}".format(frst, scnd), end=", ")
+    for scnd in range(frst + 1, 10):
+        if ((frst + scnd) != 17):
+            print("{}{}".format(frst, scnd), end=", ")
 
-print("{}, {}".format(79, 89))
+print("{}".format(89))
