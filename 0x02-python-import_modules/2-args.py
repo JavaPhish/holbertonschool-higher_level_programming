80279a6483ecb9bc76b73903e969929bd101ace7
Carter C <cclements3150@gmail.com>
2020-04-30 14:09:53 -0400

    added import fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,13 @@
 
 from sys import argv
 
-if len(argv) is 1:
-    print("0 arguments.")
-elif len(argv) is 2:
-    print("1 argument:")
-    print("1: {}".format(argv[1]))
-else:
-    print("{} arguments:".format(len(argv) - 1))
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    if len(argv) is 1:
+        print("0 arguments.")
+    elif len(argv) is 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
