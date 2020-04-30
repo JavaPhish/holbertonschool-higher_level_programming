98250776df33085e5243ff2d41e15472f9449ab5
Carter C <cclements3150@gmail.com>
2020-04-30 14:37:40 -0400

    Argv console calculator (advanced project)

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+import sys
+
+if __name__ == "__main__":
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    elif argv[2] == "+":
+        total = int(argv[1]) + int(argv[3])
+        print("{} + {} = {}".format(argv[1], argv[3], total))
+    elif argv[2] == "-":
+        difference = int(argv[1]) + int(argv[3])
+        print("{} - {} = {}".format(argv[1], argv[3], difference))
+    elif argv[2] == "/":
+        difference = int(argv[1]) + int(argv[3])
+        print("{} / {} = {}".format(argv[1], argv[3], difference))
+    elif argv[2] == "*":
+        difference = int(argv[1]) + int(argv[3])
+        print("{} * {} = {}".format(argv[1], argv[3], difference))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
