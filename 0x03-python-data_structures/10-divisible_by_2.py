06d12e119b35b5d8431e2f847219e9b14c98af47
Carter C <cclements3150@gmail.com>
2020-05-05 19:21:14 -0400

    [INIT] Returns a list of true and false depending if a list given has values divisible by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for i in my_list:
+        if i % 2 == 0:
+            new_list = new_list + [True]
+        else:
+            new_list = new_list + [False]
+
+    return new_list
