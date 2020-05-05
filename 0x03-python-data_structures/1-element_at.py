00cef82dec831abdaa671fa5329f7e55fce907b8
Carter C <cclements3150@gmail.com>
2020-05-05 18:20:21 -0400

    [FIX] Fixed index count:

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 
 
 def element_at(my_list, idx):
-    if (len(my_list) >= idx and idx >= 0):
+    if (len(my_list) - 1 >= idx and idx >= 0):
         return my_list[idx]
     else:
         return None
