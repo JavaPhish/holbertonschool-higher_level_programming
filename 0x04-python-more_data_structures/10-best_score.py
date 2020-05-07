620fe45b3fdb90d83e84dfdd896448e00ef852e9
Carter C <cclements3150@gmail.com>
2020-05-07 18:55:37 -0400

    [FIX] Changed to use Max function

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,12 +2,7 @@
 
 
 def best_score(a_dictionary):
-    best = -1
-    if a_dictionary is None:
+    if (a_dictionary is None):
         return None
 
-    for key in a_dictionary.keys():
-        if a_dictionary[key] > best:
-            best = a_dictionary[key]
-
-    return best
+    return max(a_dictionary, key=a_dictionary.get)
