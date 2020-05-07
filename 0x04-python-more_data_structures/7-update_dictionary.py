470a45a34c8adde1cad0b3219cdc263c02b40379
Carter C <cclements3150@gmail.com>
2020-05-07 18:41:21 -0400

    Fix

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,6 +2,5 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    new_dict = a_dictionary.copy()
-    new_dict[key] = value
-    return new_dict
+    a_dictionary[key] = value
+    return a_dictionary
