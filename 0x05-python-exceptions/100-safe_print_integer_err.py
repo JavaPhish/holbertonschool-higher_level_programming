4045660b11c4d7192ad67cea6ee4bab429f171da
Carter C <cclements3150@gmail.com>
2020-05-18 13:33:39 -0400

    [INIT] Prints an int with some xtra error handling

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except TypeError as error:
+        sys.stderr.write("Exception: {}\n".format(error))
+        return False
+    except ValueError as error:
+        sys.stderr.write("Exception: {}\n".format(error))
+        return False
