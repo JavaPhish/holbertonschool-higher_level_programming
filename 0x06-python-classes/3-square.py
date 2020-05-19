f86351aa81a05343bc4addb4eaf2313846db94ff
Carter C <cclements3150@gmail.com>
2020-05-19 12:02:33 -0400

    [INIT] Added area method which returns the area of the square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """ Square: a class. """
+
+    def __init__(self, size=0):
+        """ Init - init method """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ area - Returns the area of the square """
+        return self.__size * self.__size
