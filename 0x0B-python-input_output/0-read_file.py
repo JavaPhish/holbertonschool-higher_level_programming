1116fc02a3d931407da9a8512bbb6cec03626fdd
Carter C <cclements3150@gmail.com>
2020-06-03 08:27:53 -0400

    Reads the contents of a given filename DOES NOT HANDLE EXCEPTIONS

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Contains read_file """
+
+
+def read_file(filename=""):
+    """ Reads a file and prints to stdout """
+    with open(filename, 'r') as file:
+        print(file.read())
