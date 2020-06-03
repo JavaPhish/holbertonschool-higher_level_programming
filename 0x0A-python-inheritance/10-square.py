3735929c474ce513175efddeb3a951b756ccd321
Carter C <cclements3150@gmail.com>
2020-06-02 22:37:16 -0400

    Inherets from rectangle

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Inherets from Rectangle """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ square """
+
+    def __init__(self, size):
+        """ init """
+        Rectangle.integer_validator(self, "size", size)
+        self.__size = size
+
+    def area(self):
+        """ override from rectangle """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """ str """
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
