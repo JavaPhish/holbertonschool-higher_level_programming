586e1ea45e3d65d9b62c5783f625b77c66395e58
Carter C <cclements3150@gmail.com>
2020-06-02 22:12:25 -0400

    added are which just raises an exception (For now i hope)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Geometry class stuff """
+
+
+class BaseGeometry():
+    """ sup """
+
+    def area(self):
+        """ Just supposed to leave this broken lol """
+        raise Exception("area() is not implemented")
