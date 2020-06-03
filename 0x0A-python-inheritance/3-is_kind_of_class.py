ef2c279d49d2cf7722914f984785c49bf232aa34
Carter C <cclements3150@gmail.com>
2020-06-02 21:57:17 -0400

    CHecks if an object is an instance of a class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" contains is-kind_of_class """
+
+
+def is_kind_of_class(obj, a_class):
+    """ if an object is an instance of a class return True """
+    return isinstance(obj, a_class)
