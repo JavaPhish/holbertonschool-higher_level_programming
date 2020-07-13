aa5797fc74addd16e6804aa27c367f2429a9e409
Carter C <cclements3150@gmail.com>
2020-07-13 16:34:16 -0400

    Ehhhh

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- convert DB to utf8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+SELECT hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
