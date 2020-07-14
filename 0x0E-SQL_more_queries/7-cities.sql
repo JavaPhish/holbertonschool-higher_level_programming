ac37ae7c272278b0a8f41c8ace99de7829db20eb
Carter C <cclements3150@gmail.com>
2020-07-14 18:25:07 -0400

    Creates new table and database

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Creates DB and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL,
+	FOREIGN KEY (id) REFERENCES state(id),
+ 	name VARCHAR(256) NOT NULL
+);
