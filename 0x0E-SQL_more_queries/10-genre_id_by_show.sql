3c7065170cd9256605f4bf386c127f37a3f83614
Carter C <cclements3150@gmail.com>
2020-07-14 19:56:50 -0400

    Added distinct flag

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- TV show dumpy
 SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows, tv_show_genres
+FROM tv_shows, tv_show_genres WHERE tv_show_genres.genre_id >= 1
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
