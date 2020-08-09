72808ced8a018599b4efdaaa027a49f11c8988bd
CARTER_CLEMENTS <831@holbertonschool.com>
2020-08-09 10:56:09 -0400

    Search for ID by state name

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Fetch database stuff """
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    """ create the 'engine' """
+
+    """ I know this is ugly. I had to use pep8 which limits line chars """
+    eng_log = "{}:{}".format(sys.argv[1], sys.argv[2])
+    eng_s = '://{}@localhost/{}'.format(eng_log, sys.argv[3])
+    engine = create_engine('mysql+mysqldb{}'.format(eng_s), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    """ Create a session, i had to bind the engine differently """
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    """ Find the id of the state searched (argv[4]) """
+    state = session.query(State).filter_by(name=sys.argv[4]).first()
+    if state is not None:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
+
+    """ Close the session """
+    session.close()
