51a5031f5b24824c25f04eee8bf6f3f55be8f14f
JavaPhish <cclements3150@gmail.com>
2020-08-18 19:02:29 -0400

    Removed argv, specify ip in file

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Finds a secret thing through redirects and data :)
-curl -L -X PUT --data "user_id=98" -H "Origin: HolbertonSchool" "$1"
+curl -L -X PUT --data "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
