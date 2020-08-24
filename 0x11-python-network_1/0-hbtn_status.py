e1b654d0191dca42c7e29da5f87420ddc8cdcb3a
JavaPhish <cclements3150@gmail.com>
2020-08-24 14:43:03 -0400

    Trying to use \t escape character? maybe checker wants that used

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         r = response.read()
         print("Body response:")
-        print("    - type: {}".format(type(response.read())))
-        print("    - content: {}".format(r))
-        print("    - utf8 content: {}".format(r.decode('utf-8')))
+        print("\t- type: {}".format(type(response.read())))
+        print("\t- content: {}".format(r))
+        print("\t- utf8 content: {}".format(r.decode('utf-8')))
