dc37bde5e016398459e6425b27ddc49ee6292f00
JavaPhish <cclements3150@gmail.com>
2020-08-24 14:53:30 -0400

    The same as 0 but with the requests library

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Requests stuff """
+
+
+import requests
+
+if __name__ == '__main__':
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
