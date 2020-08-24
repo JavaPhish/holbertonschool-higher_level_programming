caaf3b05d1bd32cacb44493bd6248b45ef68e03f
JavaPhish <cclements3150@gmail.com>
2020-08-24 14:57:26 -0400

    Some header thing they want printed

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Requests stuff """
+
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    r = requests.get(sys.argv[1])
+    print(r.headers.get("X-Request-Id"))
