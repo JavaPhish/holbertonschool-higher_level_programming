247a4c22d811669be1d2d365bb93864ed6632b57
JavaPhish <cclements3150@gmail.com>
2020-08-24 14:09:12 -0400

    Added a line

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
 
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         r = response.read()
+        print("Body response:")
         print("    - type: {}".format(type(response.read())))
         print("    - content: {}".format(r))
         print("    - utf8 content: {}".format(r.decode('utf-8')))
