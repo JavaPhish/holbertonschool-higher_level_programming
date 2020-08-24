bfb57f997e45039e5d2274741f7b39f04054c48b
JavaPhish <cclements3150@gmail.com>
2020-08-24 14:07:31 -0400

    Fetches body of the url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Requests stuff """
+
+
+import urllib.request
+
+
+if __name__ == "__main__":
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        r = response.read()
+        print("    - type: {}".format(type(response.read())))
+        print("    - content: {}".format(r))
+        print("    - utf8 content: {}".format(r.decode('utf-8')))
