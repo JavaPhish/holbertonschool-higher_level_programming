fc05eb0da7dc1dc8562ba12cb863273fc4deb112
JavaPhish <cclements3150@gmail.com>
2020-09-15 19:59:50 -0400

    Fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
 if (process.argv.length > 3) {
-  const argN = process.argv.splice(2).map(Number);
+  let argN = process.argv.splice(2).map(Number);
+  argN = [...new Set(argN)];
   argN.sort();
   console.log(argN[argN.length - 2]);
 } else {
