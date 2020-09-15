d615959262018ab918a5738604622a1ffe3e2e62
JavaPhish <cclements3150@gmail.com>
2020-09-15 18:45:03 -0400

    Finds second highest value

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+if (Number(process.argv[2]) && Number(process.argv[2]) !== 1) {
+  let max = process.argv[2];
+  let sec = process.argv[3];
+
+  for (let x = 2; x < process.argv.length; x++) {
+    if (process.argv[x] > max) {
+      max = process.argv[x];
+    }
+  }
+  for (let x = 2; x < process.argv.length; x++) {
+    if (process.argv[x] > sec && process.argv[x] !== max) {
+      sec = process.argv[x];
+    }
+  }
+
+  console.log(sec);
+} else {
+  console.log(0);
+}
