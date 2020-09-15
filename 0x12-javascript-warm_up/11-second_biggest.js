397203649482772b3b1815def5ce477ea37cd2f9
JavaPhish <cclements3150@gmail.com>
2020-09-15 19:53:38 -0400

    :)

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,22 +1,9 @@
 #!/usr/bin/node
 
-if (Number(process.argv[2]) && Number(process.argv[2]) !== 1) {
-  let max = process.argv[2];
-  let sec = process.argv[3];
-
-  for (let x = 2; x < process.argv.length; x++) {
-    if (Number(process.argv[x]) > max) {
-      max = process.argv[x];
-    }
-  }
-
-  for (let x = 2; x < process.argv.length; x++) {
-    if (Number(process.argv[x]) > sec && process.argv[x] !== max) {
-      sec = process.argv[x];
-    }
-  }
-
-  console.log(sec);
+if (process.argv.length > 3) {
+  const argN = process.argv.splice(2).map(Number);
+  argN.sort();
+  console.log(argN[argN.length - 2]);
 } else {
   console.log(0);
 }
