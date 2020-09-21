6f3958f64344ff447c81848b06d8cfbb5ebffd35
JavaPhish <cclements3150@gmail.com>
2020-09-21 06:22:11 -0400

    Added a print function

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let x = 0; x < this.height; x++) {
+      for (let y = 0; y < this.width; y++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+}
+module.exports = Rectangle;
